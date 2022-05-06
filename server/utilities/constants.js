2dcc42ed4760d525c0a2bfce4111f46530c7cd2d
Sam Siew <Sam.Siew@Nintex.com>
2022-05-06 18:05:22 +0800

    setup app

---
+++ server/utilities/constants.js
@@ -0,0 +1,9 @@
+module.exports = {
+    authSecret: "nuh54wtglgwestrgw45tgwns",
+    tokenIssuer: "dimlucas",
+    mysqlUsername: "admin1234@my-todo",
+    mysqlPassword: "@Hello1234",
+    mysqlDatabase: "MyToDoListJS",
+    mysqlHost: "my-todo.mysql.database.azure.com",
+    mysqlPort: "3306"
+}
